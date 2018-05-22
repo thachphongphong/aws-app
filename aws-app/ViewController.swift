6e68cd3bf65f8c30ecfde9b5a86a7ef85b09fa8d
Linh Do <thachphongphong@gmail.com>
2018-05-22 12:57:07 +0700

    Initial Commit

---
+++ aws-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aws-app
+//
+//  Created by Linh Do on 5/22/18.
+//  Copyright © 2018 Linh Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
